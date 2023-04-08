31ba2c758a1f12bf89d90e8483713fe706bd7401
andre.teixeira <andre-x8@hotmail.com>
2023-04-08 09:29:06 -0300

    Inicio do projeto

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'linkedin',
+      entities: [],
+      synchronize: true,
+      logging:true,
+    }),
+    ProductModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
